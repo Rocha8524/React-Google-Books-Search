5e2c75016d3160539841d7d57c9fea956aaffcd8
Alex Rocha <rocha8524@gmail.com>
2020-01-27 17:50:33 -0500

    Added jsx

--- client/src/components/SearchForm/index.js
+++ client/src/components/SearchForm/index.js
@@ -1,28 +1,34 @@
 import React from "react";
+import "./style.css";
 
-// Exporting the Container, Row, and Col components from this file
-
-// This Container component allows us to use a bootstrap container without worrying about class names
-export function Container({ fluid, children }) {
-  return <div className={`container${fluid ? "-fluid" : ""}`}>{children}</div>;
-}
-
-// This Row component lets us use a bootstrap row without having to think about class names
-export function Row({ fluid, children }) {
-  return <div className={`row${fluid ? "-fluid" : ""}`}>{children}</div>;
-}
-
-// This Col component lets us size bootstrap columns with less syntax
-// e.g. <Col size="md-12"> instead of <div className="col-md-12">
-export function Col({ size, children }) {
+function SearchForm({ q, handleInputChange, handleFormSubmit }) {
   return (
-    <div
-      className={size
-        .split(" ")
-        .map(size => "col-" + size)
-        .join(" ")}
-    >
-      {children}
-    </div>
+    <form className="form-inline" role="form">
+      <div className="form-group mx-sm-3 mb-2">
+        <label htmlFor="Title" className="sr-only">
+          Search Book Title
+        </label>
+        <input
+          className="form-control heading-subtitle "
+          id="Title"
+          type="text"
+          value={q}
+          placeholder="Book Title..."
+          name="q"
+          onChange={handleInputChange}
+          size="55"
+          required
+        />
+      </div>
+      <button
+        onClick={handleFormSubmit}
+        type="submit"
+        className="btn btn-lg search-button heading-subtitle"
+      >
+        Search
+      </button>
+    </form>
   );
 }
+
+export default SearchForm;
