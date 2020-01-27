9be586d616ca3c356193697f86a3a6de56d09d8e
Alex Rocha <rocha8524@gmail.com>
2020-01-27 17:46:37 -0500

    Book and Footer complete

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,27 +1,12 @@
 import React from "react";
+import "./style.css";
 
-// This file exports the Input, TextArea, and FormBtn components
-
-export function Input(props) {
+const Footer = () => {
   return (
-    <div className="form-group">
-      <input className="form-control" {...props} />
-    </div>
+    <footer className="footer navbar-fixed-bottom has-background-tan text-center heading-subtitle">
+      Made with React
+    </footer>
   );
-}
+};
 
-export function TextArea(props) {
-  return (
-    <div className="form-group">
-      <textarea className="form-control" rows="20" {...props} />
-    </div>
-  );
-}
-
-export function FormBtn(props) {
-  return (
-    <button {...props} style={{ float: "right", marginBottom: 10 }} className="btn btn-success">
-      {props.children}
-    </button>
-  );
-}
+export default Footer;
